b53bfb71e2c90e67ebc25841bd8a708048ef2c2c
Albert2424 <albertixortega@gmail.com>
2024-04-15 12:28:11 +0200

    Added confidence plot and threshold features

--- DB_example/codes/plots.py
+++ DB_example/codes/plots.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib.cm as cm
 from argparse import ArgumentParser
+import os
 
 parser = ArgumentParser()
 parser.add_argument(
@@ -44,6 +45,13 @@ parser.add_argument(
     required=False,
     default=None,
 )
+parser.add_argument(
+    "--plot_th",
+    type=int,
+    help="Whether or not to only plot the points below the threshold. Default is False.",
+    required=False,
+    default=0,
+)
 args = parser.parse_args()
 
 
@@ -75,8 +83,8 @@ def plot_ab_count(dfa, dfb, threshold=0.1):
     x = np.array(dfb["Kd (nM)"])
     y = np.array(dfa["Kd (nM)"])
 
-    err_x = np.abs(np.array(dfa["kd SEM"]) / x)
-    err_y = np.abs(np.array(dfb["kd SEM"]) / y)
+    err_x = np.abs(np.array(dfb["kd SEM"]) / x)
+    err_y = np.abs(np.array(dfa["kd SEM"]) / y)
 
     mask_x = err_x != 0
     mask_y = err_y != 0
@@ -116,7 +124,7 @@ def plot_ab_count(dfa, dfb, threshold=0.1):
         x_values,
         x_values + np.log10(threshold),
         color=c_list[1],
-        label=f"$log(K_D^A/K_D^B)$ = {threshold}",
+        label=f"$log(K_D^A/K_D^B) = log({threshold}$)",
     )
 
     plt.xlabel(rf"$log(K_D^B)$", fontsize=35)
@@ -129,19 +137,21 @@ def plot_ab_count(dfa, dfb, threshold=0.1):
     plt.legend(fontsize=35)
 
     try:
-        plt.xlim([min(min(x), min(x_err)) - 0.2, max(max(x), max(x_err)) + 0.2])
-        plt.ylim([min(min(y), min(y_err)) - 0.2, max(max(y), max(y_err)) + 0.2])
+        min_value = min(min(min(x), min(x_err)), min(min(y), min(y_err))) - 0.2
+        max_value = max(max(max(x), max(x_err)), max(max(y), max(y_err))) + 0.2
     except ValueError:
-        plt.xlim([min(x) - 0.2, max(x) + 0.2])
-        plt.ylim([min(y) - 0.2, max(y) + 0.2])
+        min_value = min(min(x), min(y)) - 0.2
+        max_value = max(max(x), max(y)) + 0.2
 
+    plt.xlim([min_value, max_value])
+    plt.ylim([min_value, max_value])
     plt.tight_layout()
 
-    plt.savefig("output/figures/ka_kb_count.pdf")
+    plt.savefig("output/figures/ka_kb_count.png")
     # plt.show()
 
 
-def plot_bool_res(dfa, dfb, threshold=0.1, filename=None):
+def plot_bool_res(dfa, dfb, threshold=0.1, filename=None, plot_th=0):
     """
     This function plots the dataframes dfa and dfb, where dfa contains the affinities of proteins A with the same ligand, and dfb contains the affinities of proteins
     B with the same ligand.
@@ -170,6 +180,22 @@ def plot_bool_res(dfa, dfb, threshold=0.1, filename=None):
         else:
             dfa = dfa.drop(i)
             dfb = dfb.drop(i)
+    dfa = dfa.reset_index(drop=True)
+    dfb = dfb.reset_index(drop=True)
+
+    if threshold != 1:
+        mask = dfa[dfa["Kd (nM)"] / dfb["Kd (nM)"] >= threshold]
+        col_th = col.drop(mask.index)
+
+        col_th = np.array(col_th["Prediction"], dtype=float)
+        col_th[col_th == 0] = 0.2
+        correct_th = len(col_th[col_th == 0.2])
+        failed_th = len(col_th) - correct_th
+
+        if plot_th == 1:
+            dfa = dfa.drop(mask.index)
+            dfb = dfb.drop(mask.index)
+            col = col.drop(mask.index)
 
     col = np.array(col["Prediction"], dtype=float)
     col[col == 0] = 0.2
@@ -181,11 +207,11 @@ def plot_bool_res(dfa, dfb, threshold=0.1, filename=None):
 
     if len(x) != len(col):
         print(
-            f'{filename.split("/")[-1].split(".")[0]}_kakb_pred.pdf will not be generated since the prediction file and the data_A and data_B are not corresponding:{dfa.shape}, {len(col)}'
+            f'{filename.split("/")[-1].split(".")[0]}_kakb_pred.png will not be generated since the prediction file and the data_A and data_B are not corresponding:{dfa.shape}, {len(col)}'
         )
         return
-    err_x = np.array(dfa["kd SEM"]) / x
-    err_y = np.array(dfb["kd SEM"]) / y
+    err_x = np.array(dfb["kd SEM"]) / x
+    err_y = np.array(dfa["kd SEM"]) / y
 
     mask_x = err_x != 0
     mask_y = err_y != 0
@@ -217,7 +243,7 @@ def plot_bool_res(dfa, dfb, threshold=0.1, filename=None):
         x_values,
         x_values + np.log10(threshold),
         color=c_list[1],
-        label=f"$log(K_D^A/K_D^B)$ = {threshold}",
+        label=f"$log(K_D^A/K_D^B) = log({threshold}$)",
     )
 
     plt.xlabel(rf"$log(K_D^B)$", fontsize=35)
@@ -227,29 +253,55 @@ def plot_bool_res(dfa, dfb, threshold=0.1, filename=None):
     cbar = plt.colorbar()
     cbar.ax.tick_params(labelsize=35)
 
-    plt.legend(
-        fontsize=35,
-        markerscale=4.0,
-        handles=a.legend_elements()[0],
-        labels=[f"Guessed: {correct}", f"Failed: {failed}"],
-    )
+    if plot_th == 1:
+        legend = plt.legend(
+            fontsize=35,
+            markerscale=4.0,
+            handles=a.legend_elements()[0],
+            labels=[f"Guessed: {correct}", f"Failed: {failed}"],
+        )
+        plt.gca().add_artist(legend)
+        legend_l =plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1),
+          fancybox=True, shadow=True, ncol=2, fontsize=35)
+    else:
+        legend = plt.legend(
+            fontsize=35,
+            markerscale=4.0,
+            handles=a.legend_elements()[0],
+            labels=[f"Guessed: {correct}", f"Failed: {failed}"],
+        )
+        plt.gca().add_artist(legend)
+        if threshold != 1:
+            legend2 = plt.legend(
+                fontsize=35,
+                markerscale=4.0,
+                handles=a.legend_elements()[0],
+                labels=[f"Guessed (th): {correct_th}", f"Failed (th): {failed_th}"],
+                bbox_to_anchor=(0.455, 0.85),
+            )
+            plt.gca().add_artist(legend2)
+            legend_l =plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1),
+          fancybox=True, shadow=True, ncol=2, fontsize=35)
     cbar.set_label("DiffDock guess", fontsize=35)
     cbar.set_ticks([0, 1])
     cbar.set_ticklabels(["A", "B"])
 
     try:
-        plt.xlim([min(min(x), min(x_err)) - 0.2, max(max(x), max(x_err)) + 0.2])
-        plt.ylim([min(min(y), min(y_err)) - 0.2, max(max(y), max(y_err)) + 0.2])
+        min_value = min(min(min(x), min(x_err)), min(min(y), min(y_err))) - 0.2
+        max_value = max(max(max(x), max(x_err)), max(max(y), max(y_err))) + 0.2
     except ValueError:
-        plt.xlim([min(x) - 0.2, max(x) + 0.2])
-        plt.ylim([min(y) - 0.2, max(y) + 0.2])
+        min_value = min(min(x), min(y)) - 0.2
+        max_value = max(max(x), max(y)) + 0.2
+
+    plt.xlim([min_value, max_value])
+    plt.ylim([min_value, max_value])
 
     plt.tight_layout()
-    plt.savefig(f'output/figures/{filename.split("/")[-1].split(".")[0]}_kakb_pred.pdf')
+    plt.savefig(f'output/figures/{filename.split("/")[-1].split(".")[0]}_kakb_pred.png')
     # plt.show()
 
 
-def plot_rel_res(dfa, dfb, threshold=0.1, filename=None):
+def plot_rel_res(dfa, dfb, threshold=0.1, filename=None, plot_th=0):
     """
     This function plots the dataframes dfa and dfb, where dfa contains the affinities of proteins A with the same ligand, and dfb contains the affinities of proteins
     B with the same ligand.
@@ -277,8 +329,25 @@ def plot_rel_res(dfa, dfb, threshold=0.1, filename=None):
         else:
             dfa = dfa.drop(i)
             dfb = dfb.drop(i)
+
+    dfa = dfa.reset_index(drop=True)
+    dfb = dfb.reset_index(drop=True)
+
+    if threshold != 1:
+        mask = dfa[dfa["Kd (nM)"] / dfb["Kd (nM)"] >= threshold]
+        col_th = col.drop(mask.index)
+
+        col_th = np.array(col_th["Reliability"], dtype=float)
+        correct_th = len(col_th[col_th >= 60])
+        failed_th = len(col_th) - correct_th
+
+        if plot_th == 1:
+            dfa = dfa.drop(mask.index)
+            dfb = dfb.drop(mask.index)
+            col = col.drop(mask.index)
+
     col = np.array(col["Reliability"])
-    correct = len(col[col >= 50])
+    correct = len(col[col >= 60])
     failed = len(col) - correct
 
     x = np.array(dfb["Kd (nM)"])
@@ -286,12 +355,12 @@ def plot_rel_res(dfa, dfb, threshold=0.1, filename=None):
 
     if len(x) != len(col):
         print(
-            f'{filename.split("/")[-1].split(".")[0]}_kakb_rel.pdf will not be generated since the prediction file and the data_A and data_B are not corresponding: {dfa.shape}, {len(col)}'
+            f'{filename.split("/")[-1].split(".")[0]}_kakb_rel.png will not be generated since the prediction file and the data_A and data_B are not corresponding: {dfa.shape}, {len(col)}'
         )
         return
 
-    err_x = np.array(dfa["kd SEM"]) / x
-    err_y = np.array(dfb["kd SEM"]) / y
+    err_x = np.array(dfb["kd SEM"]) / x
+    err_y = np.array(dfa["kd SEM"]) / y
 
     mask_x = err_x != 0
     mask_y = err_y != 0
@@ -316,14 +385,16 @@ def plot_rel_res(dfa, dfb, threshold=0.1, filename=None):
     plt.errorbar(
         x_err, y_err, yerr=err_y, xerr=err_x, alpha=0.3, capsize=5, fmt="none", zorder=0
     )
-    a = plt.scatter(x, y, c=col, cmap="cividis_r", s=35, clim=(min(col), 100))  # 'RdYlGn'
+    a = plt.scatter(
+        x, y, c=col, cmap="cividis_r", s=35, clim=(min(col), max(col))
+    )  # 'RdYlGn'
 
     plt.plot(x_values, x_values, label="$log(K_D^A) = log(K_D^B)$", color=c_list[0])
     plt.plot(
         x_values,
         x_values + np.log10(threshold),
         color=c_list[1],
-        label=f"$log(K_D^A/K_D^B)$ = {threshold}",
+        label=f"$log(K_D^A/K_D^B) = log({threshold})$",
     )
 
     plt.xlabel(rf"$log(K_D^B)$", fontsize=35)
@@ -333,29 +404,61 @@ def plot_rel_res(dfa, dfb, threshold=0.1, filename=None):
     cbar = plt.colorbar()
     cbar.ax.tick_params(labelsize=35)
 
-    plt.legend(
-        fontsize=35,
-        markerscale=4.0,
-        handles=[a.legend_elements()[0][-1], a.legend_elements()[0][0]],
-        labels=[f"Reliability > 50%: {correct}", f"Reliability < 50%: {failed}"],
-    )
+    if plot_th == 1:
+        legend = plt.legend(
+            fontsize=35,
+            markerscale=4.0,
+            handles=[a.legend_elements()[0][-1], a.legend_elements()[0][0]],
+            labels=[f"Reliability > 60%: {correct}", f"Reliability < 60%: {failed}"],
+        )
+        plt.gca().add_artist(legend)
+        legend_l =plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1),
+          fancybox=True, shadow=True, ncol=2, fontsize=35)
+        # plt.gca().add_artist(legend_l)
+    else:
+        legend = plt.legend(
+            fontsize=35,
+            markerscale=4.0,
+            handles=[a.legend_elements()[0][-1], a.legend_elements()[0][0]],
+            labels=[f"Reliability > 60%: {correct}", f"Reliability < 60%: {failed}"],
+        )
+        plt.gca().add_artist(legend)
+        if threshold != 1:
+            legend2 = plt.legend(
+                fontsize=35,
+                markerscale=4.0,
+                handles=[a.legend_elements()[0][-1], a.legend_elements()[0][0]],
+                labels=[
+                    f"Reliability > 60% (th): {correct_th}",
+                    f"Reliability < 60% (th): {failed_th}",
+                ],
+                bbox_to_anchor=(0.62, 0.85),
+            )
+            plt.gca().add_artist(legend2)
+            legend_l =plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1),
+          fancybox=True, shadow=True, ncol=2, fontsize=35)
+
     cbar.set_label("DiffDock guess reliability", fontsize=35)
-    cbar.set_ticks([50, 75, 100])
-    cbar.set_ticklabels(["50%", "75%", "100%"])
+    # cbar.set_ticks([50, 75, 100])
+    # cbar.set_ticklabels(["50%", "75%", "100%"])
 
     try:
-        plt.xlim([min(min(x), min(x_err)) - 0.2, max(max(x), max(x_err)) + 0.2])
-        plt.ylim([min(min(y), min(y_err)) - 0.2, max(max(y), max(y_err)) + 0.2])
+        min_value = min(min(min(x), min(x_err)), min(min(y), min(y_err))) - 0.2
+        max_value = max(max(max(x), max(x_err)), max(max(y), max(y_err))) + 0.2
     except ValueError:
-        plt.xlim([min(x) - 0.2, max(x) + 0.2])
-        plt.ylim([min(y) - 0.2, max(y) + 0.2])
+        min_value = min(min(x), min(y)) - 0.2
+        max_value = max(max(x), max(y)) + 0.2
+
+    plt.xlim([min_value, max_value])
+    plt.ylim([min_value, max_value])
+
     plt.tight_layout()
 
-    plt.savefig(f'output/figures/{filename.split("/")[-1].split(".")[0]}_kakb_rel.pdf')
+    plt.savefig(f'output/figures/{filename.split("/")[-1].split(".")[0]}_kakb_rel.png')
     # plt.show()
 
 
-def plot_failed(dfa, dfb, failed, threshold=0.1, counts=False, predictions=None):
+def plot_failed(dfa, dfb, failed, threshold=0.1, counts=False, predictions=None, plot_th=0):
 
     try:
         fail_df = pd.read_csv(failed)
@@ -412,15 +515,46 @@ def plot_failed(dfa, dfb, failed, threshold=0.1, counts=False, predictions=None)
     predictions = predictions.split(".")[0] + "_no_fails.csv"
     if counts:
         plot_ab_count(dfa, dfb, threshold=threshold)
-    plot_bool_res(dfa, dfb, threshold=threshold, filename=predictions)
-    plot_rel_res(dfa, dfb, threshold=threshold, filename=predictions)
+    plot_bool_res(dfa, dfb, threshold=threshold, filename=predictions, plot_th=0)
+    plot_rel_res(dfa, dfb, threshold=threshold, filename=predictions, plot_th=0)
+
+
+def plot_conf_per_sample(n):
+    results = os.listdir("DD")
+
+    for r in results:
+        df = pd.read_csv("DD/" + r)
+        samples = len(df[df["ID"] == df["ID"].values[0]])
+        samp_list = np.arange(1, samples + 1)
+
+        conf = df["Confidence"].values
+        c_list = cm.viridis(np.linspace(0, 1, 8))
+        plt.figure()
+        for i in range(n):
+            y = conf[i * samples : (i + 1) * samples]
+            m = max(y)
+            y_best = y[y > m - abs(m / 2)]
+            x_best = samp_list[: len(y_best)]
+            if m >= 0:
+                c = 0
+            else:
+                c = 4
+            plt.plot(samp_list, y, color=c_list[c], alpha=0.7)
+            plt.plot(x_best, y_best, color="r")
+
+        plt.xlabel("Sample")
+        plt.ylabel("Confidence")
+        plt.title(f"DD runs: {n}")
+        plt.savefig(
+            f'output/figures/{r.split("/")[-1].split(".")[0]}_conf_per_sample_{n}.png'
+        )
+        # plt.show()
 
 
 if __name__ == "__main__":
     print(f"Plotting {args.predictions}")
     dfa = pd.read_csv(args.data_a)
     dfb = pd.read_csv(args.data_b)
-
     # dfa = dfa.loc[[i for i in range(51)]]
     # dfb = dfb.loc[[i for i in range(51)]]
 
@@ -436,10 +570,14 @@ if __name__ == "__main__":
             threshold=args.threshold,
             counts=args.counts,
             predictions=args.predictions,
+            plot_th=args.plot_th
         )
 
     else:
         if args.counts:
             plot_ab_count(dfa, dfb, threshold=args.threshold)
-        plot_bool_res(dfa, dfb, threshold=args.threshold, filename=args.predictions)
-        plot_rel_res(dfa, dfb, threshold=args.threshold, filename=args.predictions)
+            plot_conf_per_sample(10)
+            plot_conf_per_sample(20)
+            plot_conf_per_sample(30)
+        plot_bool_res(dfa, dfb, threshold=args.threshold, filename=args.predictions, plot_th=args.plot_th)
+        plot_rel_res(dfa, dfb, threshold=args.threshold, filename=args.predictions, plot_th=args.plot_th)
