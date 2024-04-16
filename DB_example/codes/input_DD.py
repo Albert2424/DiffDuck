b0715d41361e64879f98d218ce6aea0b6af3649f
juan <juangaropascual@gmail.com>
2024-04-16 12:29:01 +0200

    pandas instead csv

--- DB_example/codes/input_DD.py
+++ DB_example/codes/input_DD.py
@@ -1,25 +1,19 @@
-import csv
+import pandas as pd
 
 # Archivos de entrada y salida
-archivo_a = 'data_A.csv'
-archivo_b = 'data_B.csv'
-archivo_resultado = 'input_DD.csv'
-nombres_columnas = ['complex_name', 'chainA', 'chainB','SMILES']
-path = '/home/ramon/juan/alphafold/working_structures/'
+data_a = pd.read_csv('data_A.csv')
+data_b = pd.read_csv('data_B.csv')
 
-# Leer archivos de entrada y escribir las columnas 2 y 3 en el archivo de salida
-with open(archivo_a, 'r') as file_a, open(archivo_b, 'r') as file_b, open(archivo_resultado, 'w', newline='') as file_resultado:
-    reader_a = csv.reader(file_a)
-    reader_b = csv.reader(file_b)
-    writer = csv.writer(file_resultado)
+input_DD = {'complex_name':[], 'chainA': [], 'chainB': [], 'SMILES': []}
+path = '/home/ramon/juan/structures/kd_articles/AF_structures/'
 
-    writer.writerow(nombres_columnas)
-    next(reader_a)
-    next(reader_b)
+complex_name = data_a['Prot ID'].astype(str) + '_' + data_b['Prot ID'].astype(str) + '_' + data_a['PubChem CID'].astype(str)
 
-    # Iterar sobre las filas de ambos archivos simultáneamente y escribir en el archivo de salida
-    for row_a, row_b in zip(reader_a, reader_b):
-        # Combinar las columnas 2 y 3 de ambos archivos
-        row = [row_a[1] + '_' + row_b[1] + '_'+ row_a[6], path + row_a[1] + '.pdb', path + row_b[1] + '.pdb', row_b[3]]
-        # Escribir la fila combinada en el archivo de salida
-        writer.writerow(row)
+input_DD['complex_name'] = complex_name
+input_DD['chainA'] = path + data_a['Prot ID'].astype(str) + '.pdb'
+input_DD['chainB'] = path + data_b['Prot ID'].astype(str) + '.pdb'
+input_DD['SMILES'] = data_a['SMILES']
+
+df = pd.DataFrame(input_DD)
+df.to_csv('input_DD.csv', index=False)
+
