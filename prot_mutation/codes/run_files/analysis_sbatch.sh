6fd16068df7d508304be6c61240c18203ad0c36c
Albert2424 <albertixortega@gmail.com>
2024-05-07 12:58:59 +0200

    solved problem with elif

--- prot_mutation/codes/run_files/analysis_sbatch.sh
+++ prot_mutation/codes/run_files/analysis_sbatch.sh
@@ -7,18 +7,22 @@ if [ -z "$clust" ]; then
 
     if [ $AF == 1 ]; then
         ./codes/run_files/analysis.sh AF
-    elif [ $DF == 1 ]; then
+    fi
+    if [ $DF == 1 ]; then
         ./codes/run_files/analysis.sh DF
-    elif [ $OF == 1 ]; then
+    fi
+    if [ $OF == 1 ]; then
         ./codes/run_files/analysis.sh OF
     fi
 else
 
     if [ $AF == 1 ]; then
         sbatch -J "$run_name"_AF codes/run_files/analysis.sh AF
-    elif [ $DF == 1 ]; then
+    fi
+    if [ $DF == 1 ]; then
         sbatch -J "$run_name"_DF codes/run_files/analysis.sh DF
-    elif [ $OF == 1 ]; then
+    fi
+    if [ $OF == 1 ]; then
         sbatch -J "$run_name"_OF codes/run_files/analysis.sh OF
     fi
 fi
--- prot_mutation/codes/run_files/docking_sbatch.sh
+++ prot_mutation/codes/run_files/docking_sbatch.sh
@@ -7,17 +7,21 @@ if [ -z "$clust" ]; then
 
     if [ $AF == 1 ]; then
         ./codes/run_files/docking.sh AF
-    elif [ $DF == 1 ]; then
+    fi
+    if [ $DF == 1 ]; then
         ./codes/run_files/docking.sh DF
-    elif [ $OF == 1 ]; then
+    fi
+    if [ $OF == 1 ]; then
         ./codes/run_files/docking.sh OF
     fi
 else
     if [ $AF == 1 ]; then
         sbatch -J "$run_name"_AF codes/run_files/docking.sh AF
-    elif [ $DF == 1 ]; then
+    fi
+    if [ $DF == 1 ]; then
         sbatch -J "$run_name"_DF codes/run_files/docking.sh DF
-    elif [ $OF == 1 ]; then
-        sbatch -J "$run_name"_DF codes/run_files/docking.sh OF
+    fi
+    if [ $OF == 1 ]; then
+        sbatch -J "$run_name"_OF codes/run_files/docking.sh OF
     fi
 fi
